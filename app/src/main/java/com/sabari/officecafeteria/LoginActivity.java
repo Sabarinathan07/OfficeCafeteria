b90fe61e6fb2717f1350ff1438222d9e0e25c10b
Sabari <r.sabarinathan22@gmail.com>
2021-01-17 20:58:28 +0530

    nav menu added

--- app/src/main/java/com/sabari/officecafeteria/LoginActivity.java
+++ app/src/main/java/com/sabari/officecafeteria/LoginActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -12,6 +13,7 @@ public class LoginActivity extends AppCompatActivity {
 
     TextView tv_email,tv_password,tv_login,tv_not_hav_any_acc;
     EditText et_email,et_password;
+    Button login;
 
 
     @Override
@@ -25,6 +27,7 @@ public class LoginActivity extends AppCompatActivity {
         tv_not_hav_any_acc = findViewById(R.id.not_hav_any_acc);
         et_email = findViewById(R.id.et_email);
         et_password = findViewById(R.id.et_password);
+        login = findViewById(R.id.bt_login);
 
         tv_not_hav_any_acc.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -33,6 +36,16 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(i);
             }
         });
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i  = new Intent(LoginActivity.this, OfficeCafeteriaBottomNav.class);
+                startActivity(i);
+            }
+        });
+
+
 
 
 
--- app/src/main/java/com/sabari/officecafeteria/SignUpActivity.java
+++ app/src/main/java/com/sabari/officecafeteria/SignUpActivity.java
@@ -2,13 +2,54 @@ package com.sabari.officecafeteria;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    EditText et_firstName,et_lastName,et_OrganisationName,et_empIdNo,et_mobileNo,et_email,et_password,et_conformPassword;
+    Button signUp;
+    TextView tv_login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        et_firstName = findViewById(R.id.et_firstName);
+        et_lastName = findViewById(R.id.et_lastName);
+        et_OrganisationName = findViewById(R.id.et_organisationName);
+        et_empIdNo = findViewById(R.id.et_employeeIdNo);
+        et_mobileNo = findViewById(R.id.et_mobileNo);
+        et_email = findViewById(R.id.et_email);
+        et_password = findViewById(R.id.et_password);
+        et_conformPassword = findViewById(R.id.et_conform_password);
+
+        signUp = findViewById(R.id.bt_signUp);
+        tv_login = findViewById(R.id.already_hav_acc);
+
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getBaseContext(),OfficeCafeteriaBottomNav.class);
+                startActivity(i);
+            }
+        });
+
+        tv_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getBaseContext(),LoginActivity.class);
+                startActivity(i);
+            }
+        });
     }
+
+
+
+
 }
